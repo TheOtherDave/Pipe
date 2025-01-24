e4f5f91fc941ee5533d16d5510d631038e04320e
Dave Sweeris <davesweeris@mac.com>
2025-01-23 16:31:48 -0800

    Pipe operator

--- Sources/Pipe/Pipe.swift
+++ Sources/Pipe/Pipe.swift
@@ -1,2 +1,15 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+infix operator |
+
+public func | <T, U> (lhs: T, rhs: (T) -> U) -> U {
+    rhs(lhs)
+}
+
+public func | <T, U> (lhs: T?, rhs: (T) -> U) -> U? {
+    guard let lhs else {
+        return nil
+    }
+    return rhs(lhs)
+}
